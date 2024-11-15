c7ca742fdcefad47ed798114c94e988b293364dd
Shashank Goud <s.goud@cloudfmgroup.com>
2024-11-15 15:56:14 +0000

    optimised the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 import flowbitePlugin from "flowbite/plugin";
 
 export default {
   darkMode: "class",
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./index.html", // Main entry point
+    "./src/components/**/*.{js,jsx,ts,tsx}", // Specific directories in `src`
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/styles/**/*.{css,scss}", // Include styles if needed
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", // Flowbite-specific
+    "./src/*.{js,jsx}",
   ],
   theme: {
     extend: {},
