895a88c836d4269340c2e21cfb07dc2ccb69bbc6
Shashank Goud <s.goud@cloudfmgroup.com>
2024-11-15 15:40:35 +0000

    Captured memory leaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
   darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: ["flowbite/plugin"],
+  plugins: [flowbitePlugin],
 };
